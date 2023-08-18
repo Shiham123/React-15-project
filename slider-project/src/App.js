02db0d939b54c12b213ab1fb196c5a588a54b9f5
Shiham123 <s36651786@gmail.com>
2023-08-18 16:48:02 +0600

    slider project half way throuhg

---
+++ slider-project/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import data from './data';
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const [personIndex, setPersonIndex] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, index) => {
+          const { id, image, title, quote, name } = person;
+          let position = 'nextSlide';
+
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
