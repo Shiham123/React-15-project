ca5ef94a4ac5b89ec31204896b62271b81da0c4c
Shiham123 <s36651786@gmail.com>
2023-08-18 20:29:09 +0600

    ipsum project done

--- lorem-project/src/App.js
+++ lorem-project/src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-    <>
-      <h1>this is it</h1>
-    </>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs : </label>
+        <input
+          type="number"
+          value={count}
+          onChange={(event) => {
+            setCount(event.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Generate text
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
   );
 }
 
